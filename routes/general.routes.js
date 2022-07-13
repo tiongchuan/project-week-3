fdaab89e2750178c7d1babedeb3a5eafd0726412
ValerieLSH <lshvalerie95@outlook.com>
2022-07-13 10:11:46 +0800

    01

--- routes/general.routes.js
+++ routes/general.routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import TutorController from "../controllers/tutor.controller.js";
+import StudentController from "../controllers/student.controller.js";
 
 const router = Router();
 
@@ -9,11 +10,16 @@ router.get("/general", (req, res) => {
 
 // Instantiate the class
 const tutorController = new TutorController();
+const studentController = new StudentController();
 
 // Invoke retrieveTutor() in TutorController based on the route
 router.get("/general/tutor/:tutorId", tutorController.retrieveTutor);
+router.get("/general/student/:studentId", studentController.retrieveStudent);
+
 
 // Invoke retrieveTutors() in TutorController based on the route
 router.get("/general/tutors", tutorController.retrieveTutors);
+router.get("/general/student", studentController.retrieveStudents);
+
 
 export default router;
--- routes/protected.routes.js
+++ routes/protected.routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import TutorController from "../controllers/tutor.controller.js";
+import StudentController from "../controllers/student.controller.js";
 
 const router = Router();
 
@@ -9,14 +10,18 @@ router.get("/protected", (req, res) => {
 
 // Instantiate the class
 const tutorController = new TutorController();
+const studentController = new StudentController();
 
 // Invoke update() in TutorController based on the route
 router.post("/protected/tutor/update", tutorController.update);
+router.post("/protected/student/update", studentController.update);
 
 // Invoke delete() in TutorController based on the route
 router.delete("/protected/tutor/delete/:tutorId", tutorController.delete);
+router.delete("/protected/student/delete/:studentId", studentController.delete);
 
 // // Invoke add() in TutorController based on the route
-// router.put("/protected/tutor/add", tutorController.add);
+router.put("/protected/tutor/add", tutorController.add);
+router.put("/protected/student/add", studentController.add);
 
 export default router;
